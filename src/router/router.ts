cc63bbdf99f1cb11d03f744aefa64206ced34673
UrakFerraz <urakmkt@yahoo.com.br>
2022-03-08 02:04:00 -0300

    melhoria para web vitals - configura no router os components com lazy loading

---
+++ src/router/router.ts
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+	{
+		name: 'home',
+		path: '/',
+		components: {
+			default: () => import(/* webpackChunkName: "Home" */ '../pages/home.vue'),
+			header: () => import(/* webpackChunkName: "Header" */ '../components/molecule/header.vue'),
+			footer: () => import(/* webpackChunkName: "Footer" */ '../components/molecule/footer.vue')
+		}
+	},
+	{
+		name: 'movie',
+		path: '/movie/:movieId',
+		components: {
+			default: () => import(/* webpackChunkName: "MoviePage" */ '../pages/movie.vue'),
+			header: () => import(/* webpackChunkName: "Header" */ '../components/molecule/header.vue'),
+			footer: () => import(/* webpackChunkName: "Footer" */ '../components/molecule/footer.vue')
+		},props: {default: true,header: false,footer: false } },
+	{
+		name: 'genre',
+		path: '/:genre/:pageNumber',
+		components: {
+			default: () => import(/* webpackChunkName: "Genre" */ '../pages/genre.vue'),
+			header: () => import(/* webpackChunkName: "Header" */ '../components/molecule/header.vue'),
+			footer: () => import(/* webpackChunkName: "Footer" */ '../components/molecule/footer.vue')
+		},props: { default: true, header: false, footer: false }
+	},
+	{
+		name: 'cast',
+		path: '/cast/:castPersonId',
+		components: {
+			default: () => import(/* webpackChunkName: "Cast" */ '../pages/cast.vue'),
+			header: () => import(/* webpackChunkName: "Header" */ '../components/molecule/header.vue'),
+			footer: () => import(/* webpackChunkName: "Footer" */ '../components/molecule/footer.vue')
+		}, props: { default: true, header: false, footer: false }
+	}
+]
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes
+})
+
+export default router
