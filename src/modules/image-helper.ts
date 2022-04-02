b46d4c051fb07f038c850c2dbdfa4645a1a7ad9e
UrakFerraz <urakmkt@yahoo.com.br>
2022-04-02 13:02:40 -0300

    ajusta data-image
    remove tipo any e troca para cor rgba

--- src/modules/image-helper.ts
+++ src/modules/image-helper.ts
@@ -1,5 +1,3 @@
-
-import Image from '../interfaces/image-inerface'
-export default function imgSrc(prefix: string, props: any) {
-  return (props !== null && typeof props !== "undefined") ? prefix + props : "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'%3e%3cpath fill='%23990000' d='M256 8C119.034 8 8 119.033 8 256s111.034 248 248 248 248-111.034 248-248S392.967 8 256 8zm130.108 117.892c65.448 65.448 70 165.481 20.677 235.637L150.47 105.216c70.204-49.356 170.226-44.735 235.638 20.676zM125.892 386.108c-65.448-65.448-70-165.481-20.677-235.637L361.53 406.784c-70.203 49.356-170.226 44.736-235.638-20.676z'/%3e%3c/svg%3e"
+export default function imgSrc(prefix: string, props: string | null | undefined) {
+  return (props !== null && typeof props !== "undefined") ? prefix + props : "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' width='100' height='100'%3e%3cpath fill='rgba(255,0,0,0.2)' d='M256 8C119.034 8 8 119.033 8 256s111.034 248 248 248 248-111.034 248-248S392.967 8 256 8zm130.108 117.892c65.448 65.448 70 165.481 20.677 235.637L150.47 105.216c70.204-49.356 170.226-44.735 235.638 20.676zM125.892 386.108c-65.448-65.448-70-165.481-20.677-235.637L361.53 406.784c-70.203 49.356-170.226 44.736-235.638-20.676z'/%3e%3c/svg%3e"
 }
