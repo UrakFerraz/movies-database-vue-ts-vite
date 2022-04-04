d811855d603ed0e2ec2b70b4a76d22232a605720
UrakFerraz <urakmkt@yahoo.com.br>
2022-04-03 23:05:37 -0300

    escreve + testes unitários

--- src/modules/movies-dp-api.spec.ts
+++ src/modules/movies-dp-api.spec.ts
@@ -1,44 +1,68 @@
-it('should be TMDB url prefix', () => {
-	// const testApi = new MovieDatabase(470)
-	expect('https://api.themoviedb.org/3').toBe('https://api.themoviedb.org/3')
-});
+import TMDB from './tmdb'
+import MovieDatabase from './movies-db-api'
 
-// it('should be API_KEY', () => {
-// 	expect(testApi.API_KEY).toBe()
-// })
-// it('should ', () => {
-// 	expect(testApi.defaultLanguage).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.genresListUrl).toBe()	
-// });
-// it('should ', () => {
-// 	expect(testApi.nowPlayingMoviesUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.popularMoviesUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.topRatedMoviesUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.upcomingMoviesUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.movieDetailsUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.movieCreditsUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.similarMoviesUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.movieImagesUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.movieVideosUrl).toBe()
-// });
-// it('should ', () => {
-// 	expect(testApi.movieExternalIdsUrl).toBe()
-// });
+const testApi = new MovieDatabase(470)
+
+describe('deve testar os parâmetros das URLS', () => {
+	it('deve ser o prefixo da url do TMDB', () => {
+		expect(testApi.apiURL).toBe('https://api.themoviedb.org/3')
+	});
+
+	it('deve ser a string de API_KEY', () => {
+		expect(testApi.API_KEY).toBe(TMDB)
+	})
+
+	it('deve ser a linguagem padrão', () => {
+		expect(testApi.defaultLanguage).toBe('language=pt-BR')
+	});
+})
+
+describe('deve testas as urls', () => {
+	it('deve ser a url de lista de gêneros', () => {
+		expect(testApi.genresListUrl).toBe(`https://api.themoviedb.org/3/genre/movie/list?api_key=${TMDB}&language=pt-BR`)
+	});
+	
+	it('deve ser a url dos filmes atuais', () => {
+		expect(testApi.nowPlayingMoviesUrl).toBe(`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB}&language=pt-BR&page=1`)
+	});
+
+	it('deve ser a url de filmes + populares', () => {
+		expect(testApi.popularMoviesUrl).toBe(`https://api.themoviedb.org/3/movie/popular?api_key=${TMDB}&language=pt-BR&page=1`)
+	});
+
+	it('deve ser a url de filmes + votaods', () => {
+		expect(testApi.topRatedMoviesUrl).toBe(`https://api.themoviedb.org/3/movie/top_rated?api_key=${TMDB}&language=pt-BR&page=1`)
+	});
+
+	it('deve ser a url de próximos lançamentos', () => {
+		expect(testApi.upcomingMoviesUrl).toBe(`https://api.themoviedb.org/3/movie/upcoming?api_key=${TMDB}&language=pt-BR&page=1`)
+	});
+
+	it('deve ser a url dos detalhes dos filmes', () => {
+		expect(testApi.movieDetailsUrl).toBe(`https://api.themoviedb.org/3/movie/${testApi.movieId}?api_key=${TMDB}&language=pt-BR`)
+	});
+
+	it('deve ser a url dos créditos dos filmes', () => {
+		expect(testApi.movieCreditsUrl).toBe(`https://api.themoviedb.org/3/movie/${testApi.movieId}/credits?api_key=${TMDB}&language=pt-BR`)
+	});
+
+	it('deve ser a url de filmes similares', () => {
+		expect(testApi.similarMoviesUrl).toBe(`https://api.themoviedb.org/3/movie/${testApi.movieId}/similar?api_key=${TMDB}&language=pt-BR&page=1`)
+	});
+
+	it('deve ser a url de imagens do filme', () => {
+		expect(testApi.movieImagesUrl).toBe(`https://api.themoviedb.org/3/movie/${testApi.movieId}/images?api_key=${TMDB}&language=pt-BR`)
+	});
+
+	it('deve ser a url de videos dos filmes ', () => {
+		expect(testApi.movieVideosUrl).toBe(`https://api.themoviedb.org/3/movie/${testApi.movieId}/videos?api_key=${TMDB}&language=pt-BR`)
+	});
+
+	it('deve ser a url de ids externos', () => {
+		expect(testApi.movieExternalIdsUrl).toBe(`https://api.themoviedb.org/3/movie/${testApi.movieId}/extenal_ids?api_key=${TMDB}`)
+	});
+
+	it('deve ser a url de gênero', () => {
+		expect(testApi.getGenreUrl(1, '35')).toBe(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=35`)
+	})
+});
