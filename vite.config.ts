de81c24eedc00649d44ab59aa881e943c193fb94
UrakFerraz <urakmkt@yahoo.com.br>
2022-04-09 02:27:47 -0300

    ajusta as dependências do jest no projeto

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
           @import "./src/assets/_colors.scss";
+          @import "./src/assets/_mixins.scss";
         `
       }
     }
